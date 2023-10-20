db2482765140d205b0d2d8befae32055db1ab6b1
zzyyds <zzyyds@umich.edu>
2023-10-19 21:36:34 -0400

    writting Ex state

--- src/PE_CNTL.sv
+++ src/PE_CNTL.sv
@@ -1,31 +1,55 @@
 `timescale 1ns/100ps
 module PE_CNTL
-#(parameter Wt=60,Ht=60, PE_num=1)(
+#(parameter PE_num=1)(
 //-------------------Input-------------------------//
     input clk,
     input rst,
-    filter_Parameter filter_Parameter_TB,
+    PPU_to_CNTL PPU_to_CNTL_in,
+    Conv_filter_Parameter Conv_filter_Parameter_TB,
     input_activation_Parameter input_activation_Parameter_PPU,
+    Response_Stream_PE  Response_Stream_PE_in,
+    input Stream_filter_finish,//From Top, Response_Stream_Complete packet
+    input Stream_input_finish_PE,
 
+//--------------------output------------------------//
     output Req_Stream Req_Stream_PE;
 
 
 
 );
-parameter a1_Boundary=Wt*Ht/`I1, N=4, IDLE='d0, Stream_Conv_Layer='d1, Ex_Conv_Layer='d3, Conv_Layer_PPU='d4;
-logic[$clog2(N)-1:0] state,nx_state;
+parameter  N=4, IDLE='d0, Stream_Conv_Layer='d1, Ex_Conv_Layer='d3, Conv_Layer_PPU='d4;
+logic[$clog2(N)-1:0] state, nx_state;
+logic Current_data_flow;
 logic[$clog2(`num_of_Conv_Layer)-1:0] Current_Conv_Layer, nx_Conv_Layer;
-logic[$clog2(`max_num_filter)-1:0] Current_k, nx_k;
+logic[$clog2(`max_num_K)-1:0] Current_k, nx_k;
+logic[$clog2(`max_num_channel)-1:0] Current_c, nx_c;
+logic[$clog2(`max_num_Wt*`max_num_Ht/`I)-1:0] Current_a, nx_a;
+logic[$clog2(`Kc*max_num_R*max_num_S/`F):0] Current_w, nx_w;
+logic Partial_w, Partial_a, Partial_c, Partial_k;
+logic[$clog2(`max_num_K)-1:0] reg_k_Conv_Boundary;
+logic[$clog2(`Kc*max_num_R*max_num_S/`F)-1:0] reg_w_Conv_Boundary;
+logic[$clog2(`max_num_channel)-1:0] reg_c_Conv_Boundary;
+logic[$clog2(`max_num_Wt*max_num_Ht/`I)-1:0] reg_a_Conv_Boundary;
 always_ff@(posedge clk)begin
     if(rst)begin
         state<=#1 'd0;
         Current_Conv_Layer<=#1 'd0;
         Current_k<=#1 'd0;
+        Current_data_flow<=#1 'd1;
+        reg_k_Conv_Boundary<=#1 'd0;
+        reg_w_Conv_Boundary<=#1 'd0;
+        reg_c_Conv_Boundary<=#1 'd0;
+        reg_a_Conv_Boundary<=#1 'd0;
     end
     else begin
         state<=#1 nx_state;
         Current_Conv_Layer<=#1 nx_Conv_Layer;
         Current_k<=#1 nx_k;
+        Current_data_flow<=#1 PPU_to_CNTL_in.data_flow_PPU;
+        reg_k_Conv_Boundary<=#1 Conv_filter_Parameter_TB.k_Conv_Boundary;
+        reg_w_Conv_Boundary<=#1 Conv_filter_Parameter_TB.w_Conv_Boundary;
+        reg_c_Conv_Boundary<=#1 Conv_filter_Parameter_TB.c_Conv_Boundary;
+        reg_a_Conv_Boundary<=#1 Current_data_flow? PPU_to_CNTL_in.num_of_compressed_data : Conv_filter_Parameter_TB.a_Conv_Boundary;
     end
 end
 always_comb begin
@@ -39,13 +63,41 @@ always_comb begin
             else begin
                 nx_state=IDLE;
             end
-        Stream_Conv_Layer: 
-            Req_Stream_PE.Req_Stream_filter_valid='d1;
-            Req_Stream_PE.Req_Stream_filter_k=Current_k;
-            Req_Stream_PE.Req_Stream_Conv_Layer_num=Current_Conv_Layer;
-            Req_Stream_PE.Req_Stream_input_valid='d1;
+        Stream_Conv_Layer:
+            if(Current_Conv_Layer==0)begin
+                Req_Stream_PE.Req_Stream_filter_valid='d1;
+                Req_Stream_PE.Req_Stream_filter_k=Current_k;
+                Req_Stream_PE.Req_Stream_Conv_Layer_num=Current_Conv_Layer;
+                Req_Stream_PE.Req_Stream_input_valid='d1;
+                if(Stream_filter_finish && Stream_input_finish_PE)begin
+                    nx_state=Ex_Conv_Layer;
+                end
+                else begin
+                    nx_state=Stream_Conv_Layer;
+                end
+            end
+            else begin
+                Req_Stream_PE.Req_Stream_filter_valid='d1;
+                Req_Stream_PE.Req_Stream_filter_k=Current_k;
+                if(Stream_filter_finish)begin
+                    nx_state=Ex_Conv_Layer;
+                end
+                else begin
+                    nx_state=Stream_Conv_Layer;
+                end
+            end
+
+        Ex_Conv_Layer:
+            if(Current_data_flow)begin //sparse data flow
+
+
+            end
+            else begin// dense data flow
+
+            end
+
         default: nx_state=IDLE;
-            
+
         
     endcase
 end
--- src/sys_defs.svh
+++ src/sys_defs.svh
@@ -1,28 +1,33 @@
 `define num_of_PE 4
+`define max_num_K 384//k_Conv_Boundary
 `define max_num_filter 384
+`define max_num_channel 384
+`define max_num_S 11
+`define max_num_R 11//w_Conv_Boundary
+`define max_size_output 55 // one dimension
+`define max_num_Wt 60 // for caculating a boundary
+`define max_num_Ht 60
 `define num_of_Conv_Layer 5
-// `define num_of_filters_1 96//K1
 `define Kc 4
-//`define k1_Boundary `num_of_filters_1/`Kc-1 //k1'
-`define C1 3
+`define F 4
+`define I 4
 
-`define R1 11
-`define S1 11
-`define F1 `Kc*`S1 //can be defined random
-`define w1_Boundary `Kc*`R1*`S1/`F1//=R1
-`define I1 `F1 //I1=F1, maybe better for flow control
-`define c1_Boundary `C1-1
+
+
 `define clock_frequency 1       //unit GHZ
 `define num_data 50*8/`clock_frequency/16 //8(Bytes), 16 bits for one data
 
+
 typedef struct packed {
-    logic[$clog2(55):0] W; //W of feature map//max for num  of W in alexnet is 55 for conv layer
-    logic[$clog2(55):0] H;
-    logic[$clog2(384):0] C;
-} input_activation_Parameter;//used in PPU
+   logic data_flow_PPU; //from ppu to decide which data flow is needed, 0 for dense data flow, 1 for sparse data flow
+   logic[$clog2(`max_size_output)-1:0] num_of_compressed_data;//number of data after compression
+} PPU_to_CNTL;
 typedef struct packed {
-    logic[$clog2(num_of_Conv_Layer)-1:0][$clog2(384):0] k_Conv_Boundary;//calculate in testbench
-} filter_Parameter;
+    logic[$clog2(`num_of_Conv_Layer)-1:0][$clog2(`max_num_K)-1:0] k_Conv_Boundary;//calculate in testbench
+    logic[$clog2(`num_of_Conv_Layer)-1:0][$clog2(`Kc*max_num_R*max_num_S/`F)-1:0] w_Conv_Boundary;
+    logic[$clog2(`num_of_Conv_Layer)-1:0][$clog2(`max_num_channel)-1:0] c_Conv_Boundary;
+    logic[$clog2(`num_of_Conv_Layer)-1:0][$clog2(`max_num_Wt*max_num_Ht/`I)-1:0] a_Conv_Boundary;
+} Conv_filter_Parameter;
 typedef struct packed {
     logic Req_Stream_filter_valid;
     logic[$clog2(`max_num_filter)-1:0] Req_Stream_filter_k;//filter k
@@ -35,17 +40,17 @@ typedef struct packed {
     logic[`num_data-1:0] Response_Stream_input_valid;
     logic[`num_data-1:0][15:0] Response_Stream_input_data;
     logic[$clog2(`num_of_PE)-1:0] Response_for_PE_num; 
-
-    logic Stream_filter_finish;//brodcast for all PE
-    logic[$clog2(`num_of_PE)-1:0] Stream_input_finish_PE_num;
 } Response_Stream_PE;
+typedef struct packed {
+    logic Stream_filter_finish;//brodcast for all PE
+    logic[`num_of_PE-1:0] Stream_input_finish_PE;
+} Response_Stream_Complete;//Top
+
 typedef struct packed {
     logic[`num_data-1:0] Response_Stream_filter_valid;
     logic[`num_data-1:0][15:0] Response_Stream_filter_data;
     logic[`num_data-1:0] Response_Stream_input_valid;
     logic[`num_data-1:0][15:0] Response_Stream_input_data;
     logic[`num_of_PE-1:0][$clog2(`num_of_PE)-1:0] Response_for_PE_num; //may same data give 2 PE, 
-    logic Stream_filter_finish;//brodcast for all PE
-    logic[`num_of_PE-1:0] Stream_input_finish_PE_num;
 } Response_Stream_Top;//Top use,not PE use
  
