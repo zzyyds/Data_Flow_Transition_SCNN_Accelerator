1117c73f2b1f3fda59ed19c544f0a543daf52cb2
zechuanding <143833409+zechuanding@users.noreply.github.com>
2023-11-22 12:49:06 -0500

    Xbar update

--- src/Xbar.sv
+++ src/Xbar.sv
@@ -1,30 +1,30 @@
-//`include "sys_defs.svh"
+`include "sys_defs.svh"
 
 module Xbar (
-    input   logic                   clock, 
-    input   logic                   reset,
+    input   logic                                           clock, 
+    input   logic                                           reset,
     
     // Interface with PE
-    input   DATA_PACKET             in_packet [`NUM_SRC-1:0],
-    output  logic                   busy,
+    input   DATA_PACKET [`NUM_SRC-1:0]                      in_packet ,
+    output  logic                                           busy,
 
     // Inteface with acummulate buffer
-    output  DATA_PACKET             out_packet [`NUM_DST-1:0],
-    output  logic [`NUM_DST-1:0]    out_valid                 // Output valid flag
+    output  DATA_PACKET [`NUM_DST-1:0]                      out_packet ,
+    output  logic       [`NUM_DST-1:0]                      out_valid                 // Output valid flag
     
 );  
     
     DATA_PACKET [`NUM_DST-1:0][`FIFO_DEPTH-1:0]             out_fifo;
-    logic       [`NUM_DST-1:0][$clog2(`FIFO_DEPTH)-1:0]       rd_ptr;
-    logic       [`NUM_DST-1:0][$clog2(`FIFO_DEPTH)-1:0]       wt_ptr;
+    logic       [`NUM_DST-1:0][$clog2(`FIFO_DEPTH)-1:0]     rd_ptr;
+    logic       [`NUM_DST-1:0][$clog2(`FIFO_DEPTH)-1:0]     wt_ptr;
 
     DATA_PACKET [`NUM_DST-1:0][`FIFO_DEPTH-1:0]             next_out_fifo;
-    logic       [`NUM_DST-1:0][$clog2(`FIFO_DEPTH)-1:0]       next_rd_ptr;
-    logic       [`NUM_DST-1:0][$clog2(`FIFO_DEPTH)-1:0]       next_wt_ptr;
+    logic       [`NUM_DST-1:0][$clog2(`FIFO_DEPTH)-1:0]     next_rd_ptr;
+    logic       [`NUM_DST-1:0][$clog2(`FIFO_DEPTH)-1:0]     next_wt_ptr;
 
     logic       [`NUM_DST-1:0]                              fifo_busy;      // busy means more than one valid entry in this fifo
-    logic       [`NUM_DST-1:0]                              fifo_full;      // full means this fifo is full
-    logic                                                   full;
+    //logic       [`NUM_DST-1:0]                              fifo_full;      // full means this fifo is full
+    //logic                                                   full;
 
     always_ff @(posedge clock) begin
         if (reset) begin
@@ -38,48 +38,52 @@ module Xbar (
         end
     end
 
-    logic [`NUM_DST-1:0] push_mask;
-    logic [$clog2(`NUM_DST)-1:0]    idx;
+
+    logic [$clog2(`NUM_DST)-1:0]    idx;                // Output fifo select
 
     always_comb begin
         
         // Default
         next_out_fifo   = out_fifo;
         next_wt_ptr     = wt_ptr;
+        next_rd_ptr     = rd_ptr;
 
         for (int j = 0; j < `NUM_DST; j++) begin
             if (rd_ptr[j] != wt_ptr[j]) begin
                 next_rd_ptr[j] = (rd_ptr[j] + 1) % `FIFO_DEPTH;      // Pop one packet unless empty  
-            end else begin
-                next_rd_ptr[j] = rd_ptr[j];
-            end
+            end 
         end
 
         idx = 0;
 
-
-        // Push input data packet to output fifo
-        for (int i = 0; i < `NUM_SRC; i++) begin
-            if (in_packet[i].valid) begin
-                idx = in_packet[i].index % `NUM_DST;
-                next_out_fifo[idx][next_wt_ptr[idx]] = in_packet[i];
-                next_wt_ptr[idx] = (next_wt_ptr[idx] + 1) % `FIFO_DEPTH;
+        if (~busy) begin
+            // Push input data packet to output fifo
+            for (int i = 0; i < `NUM_SRC; i++) begin
+                if (in_packet[i].valid) begin
+                    idx = in_packet[i].y % `NUM_DST;                        // Select which output fifo to go to
+                    next_out_fifo[idx][next_wt_ptr[idx]] = in_packet[i];
+                    next_wt_ptr[idx] = (next_wt_ptr[idx] + 1) % `FIFO_DEPTH;
+                end
             end
         end
     end
 
-    genvar k, l;
+
+    int num_entry [`NUM_DST-1:0];
+
+    genvar l;
     generate
         for (l = 0; l < `NUM_DST; l++) begin
             assign out_packet[l] = out_fifo[l][rd_ptr[l]];
-            assign out_valid[l]  = rd_ptr[l] != wt_ptr[l];      // If not empty, then there must be data available
-            assign fifo_busy[l]  = (rd_ptr[l] != wt_ptr[l]) & ((rd_ptr[l] + 1) != wt_ptr[l]);       // More than one entries in fifo
-            assign fifo_full[l]  = (wt_ptr + 1) == rd_ptr;  // When the two pointers are different except for the overflow bit
+            assign out_valid[l]  = rd_ptr[l] != wt_ptr[l];                  // If not empty, then there must be data available
+            assign num_entry[l]  = (wt_ptr[l] < rd_ptr[l]) ? (wt_ptr[l] + `FIFO_DEPTH - rd_ptr[l]) : (wt_ptr[l] - rd_ptr[l]);
+            assign fifo_busy[l]  = num_entry[l] >= (`FIFO_DEPTH/2);          // If the fifo is more than halfly full, assert busy and make the multipliers stall
+            //assign fifo_full[l]  = (wt_ptr + 1) == rd_ptr;                  // When the two pointers are different except for the overflow bit (currently not used)
         end
     endgenerate
 
 
     assign busy = |fifo_busy;
-    assign full = |fifo_full;
+    //assign full = |fifo_full;
 
 endmodule
